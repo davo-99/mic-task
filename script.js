da037038cf70d1a716303f723bf4e4516d60a7c3
David Movsisyan <35527153+davo1999@users.noreply.github.com>
2018-05-03 19:40:59 +0400

    Updated++

--- script.js
+++ script.js
@@ -5,19 +5,8 @@ const selected = document.getElementById('select');
 const flag1 = document.getElementById('flag1');
 const flag2 = document.getElementById('flag2');
 const flag3 = document.getElementById('flag3');
-// let arr = [];
 
-// let xhr = new XMLHttpRequest();
-// xhr.onload = function() {
-//     arr = JSON.parse(xhr.responseText);
-// }
-// xhr.onerror = function() {
-//     console.error('Error occurred.');
-// }
-// xhr.open('GET', 'map.json');
-// xhr.send();
-
-let removeDuplicates = (myArr, prop) =>
+const removeDuplicates = (myArr, prop) =>
     myArr.filter((obj, pos, arr) =>
         arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos);
 
@@ -25,22 +14,24 @@ arr = removeDuplicates(arr, 'country');
 arr.sort((a,b)=>(a.country>b.country)?1:((b.country>a.country)?-1:0)); 
 console.dir(arr);
 
-window.onload = function() {
+(() => {
     for (const elem of arr) {
-        let select = document.getElementById('select');
-        let option = document.createElement('option');
+        const select = document.getElementById('select');
+        const option = document.createElement('option');
         option.text = elem.country;
         select.add(option);
     }
-};
+})();
 
 function opFlag(opt) {
     for (let i = 0; i < arr.length; i++)
         if (opt[i].text === opt.value) {
+            [
+                flag1.style.backgroundColor,
+                flag2.style.backgroundColor,
+                flag3.style.backgroundColor
+            ] = arr[i].flag;
             console.log(`%c${opt[i].text}`, 'color: blue; font-size: 13px');
-            flag1.style.backgroundColor = arr[i].flag[0];
-            flag2.style.backgroundColor = arr[i].flag[1];
-            flag3.style.backgroundColor = arr[i].flag[2];
             break;
         }
 }
